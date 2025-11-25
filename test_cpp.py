54aeefeb2e3d000d11ee88e0cd34f492a2fdfa71
Paulobsufpe <paulo.bsimoes@ufpe.br>
2025-11-24 23:15:52 -0300

    improve testing

--- test_cpp.py
+++ test_cpp.py
@@ -1,5 +1,12 @@
+import qiskit
 import qext_cpp
 
-print(qext_cpp.build_observable())
+obs = qext_cpp.build_observable()
+print("SparseObservable instance?", isinstance(obs, qiskit.quantum_info.SparseObservable))
+print(obs)
+
 qc = qext_cpp.test()
-qc.draw(output="mpl")
+# qc.draw(output="text")
+
+print(type(obs))
+print(type(qc))
